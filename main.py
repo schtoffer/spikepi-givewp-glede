c1590a54cd994429e2a166c326cff9d658eb3baf
Christoffer Andersen <schtoffer@gmail.com>
2023-11-19 23:03:49 +0100

    Adjustet tower for 100% according to last years total sum 58,2 M

--- main.py
+++ main.py
@@ -4,43 +4,60 @@ from dotenv import load_dotenv
 import os
 import time
 
-from buildhat import ForceSensor
-
-button_left = ForceSensor('C')
-button_right = ForceSensor('D')
-
-load_dotenv()
-api_key = os.getenv("KEY")
-base_url = 'https://giver.kirkensbymisjon.no'
-response = requests.get(f'{base_url}/ws-julekampanje?x-api-key={api_key}')
-
-# Convert the text response to JSON
-json_data = json.loads(response.text) 
+from buildhat import ForceSensor, Motor
+from buildhat.exc import BuildHATError  # Correctly importing the BuildHATError
 
 from spike_functions import show_on_gauge, show_on_tower
 
 def main():
+    # print(get_profundo_data())
+    button_left = initialize_sensor('C')
+    button_right = initialize_sensor('D')
 
-    # Print values for all valid keys
-    valid_keys = ['i_antall_i_aar', 'm_sum_i_aar', 'm_max_i_aar', 'i_antall_i_fjor', 'm_sum_i_fjor', 'm_max_i_fjor', 'n_prosent_sum', 'n_prosent_antall', 'i_antall']
-    for key in valid_keys:
-        print(f"{key}: {get_profundo_data(key)}")
+    # profundo_data = {'i_antall_i_aar': 17894.0, 'm_sum_i_aar': 8225758.0, 'm_max_i_aar': 317000.0, 'i_antall_i_fjor': 23348.0, 'm_sum_i_fjor': 9836933.0, 'm_max_i_fjor': 100000.0, 'n_prosent_sum': -16.38, 'n_prosent_antall': -23.36, 'i_antall': -23.36}
+    sales_goal = 60000000
 
     while True:
         if button_left.is_pressed():
-            time.sleep(1)
-            if button_left.get_force() > 50:
-                show_on_gauge(0)
-                show_on_tower(0)
-                break
-            else:
-                show_on_gauge(get_profundo_data('n_prosent_sum'))
-                show_on_tower((get_profundo_data('m_sum_i_aar') / 45000000)*100)
-                print(f"prosent {show_on_tower((get_profundo_data('m_sum_i_aar') / 45000000)*100)}")
+            print("Waiting for Profoundo-data")
+            profundo_data = get_profundo_data()
+            print("Profoundo-data revieved")
+            achived_target = round((profundo_data['m_sum_i_aar'] / sales_goal) * 100)
+            yoy_growth = profundo_data['n_prosent_sum']
             
+            print(profundo_data)
+            show_on_tower(achived_target)
+            show_on_gauge(yoy_growth)
+            print("Tower and gaughe is updated")
+            time.sleep(10)
+            
+            show_on_tower(0)
+            show_on_gauge(0)
+            print("Tower and gaughe is reset to zero")
+        if button_right.is_pressed():
 
+            show_on_tower(0)
+            show_on_gauge(0)
+    
+def initialize_sensor(port, attempts=5, delay=5):
+    while attempts > 0:
+        try:
+            sensor = ForceSensor(port)
+            return sensor
+        except BuildHATError:
+            attempts -= 1
+            time.sleep(delay)  # Wait for 5 seconds before retrying
+    raise  # Re-raise the last exception after all attempts fail
 
-def get_profundo_data(key_str):
+      
+def get_profundo_data():
+    load_dotenv()
+    api_key = os.getenv("KEY")
+    base_url = 'https://giver.kirkensbymisjon.no'
+    response = requests.get(f'{base_url}/ws-julekampanje?x-api-key={api_key}')
+
+    # Convert the text response to JSON
+    json_data = json.loads(response.text) 
     # Check if the request was successful
     if response.status_code == 200:
         # Convert the text response to JSON
@@ -55,7 +72,7 @@ def get_profundo_data(key_str):
                         
             # print(normalized_data)
 
-        return normalized_data[key_str]
+        return normalized_data
     else:
         return f'Failed to retrieve data. Status Code: {response.status_code}'
 

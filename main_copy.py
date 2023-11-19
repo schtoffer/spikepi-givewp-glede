76161996162cee6cef158aa558efcdc03d01ff8c
Christoffer Andersen <schtoffer@gmail.com>
2023-11-19 15:58:03 +0100

    Fixed some stuff

--- main_copy.py
+++ main_copy.py
@@ -4,43 +4,46 @@ from dotenv import load_dotenv
 import os
 import time
 
-from buildhat import ForceSensor
+from buildhat import ForceSensor, Motor
+from buildhat.exc import BuildHATError  # Correctly importing the BuildHATError
 
 from spike_functions import show_on_gauge, show_on_tower
 
-sales_goal = 60000000
-
-
 def main():
     # print(get_profundo_data())
-    button_left = initialize_spike_module(ForceSensor, 'C')
-    button_right = initialize_spike_module(ForceSensor, 'D')
+    button_left = initialize_sensor('C')
+    button_right = initialize_sensor('D')
 
-    profundo_data = {'i_antall_i_aar': 17894.0, 'm_sum_i_aar': 8225758.0, 'm_max_i_aar': 317000.0, 'i_antall_i_fjor': 23348.0, 'm_sum_i_fjor': 9836933.0, 'm_max_i_fjor': 100000.0, 'n_prosent_sum': -16.38, 'n_prosent_antall': -23.36, 'i_antall': -23.36}
-    # profundo_data = get_profundo_data()
+    # profundo_data = {'i_antall_i_aar': 17894.0, 'm_sum_i_aar': 8225758.0, 'm_max_i_aar': 317000.0, 'i_antall_i_fjor': 23348.0, 'm_sum_i_fjor': 9836933.0, 'm_max_i_fjor': 100000.0, 'n_prosent_sum': -16.38, 'n_prosent_antall': -23.36, 'i_antall': -23.36}
+    profundo_data = get_profundo_data()
 
+    sales_goal = 60000000
+    print(sales_goal)
     achived_target = round((profundo_data['m_sum_i_aar'] / sales_goal) * 100)
     yoy_growth = profundo_data['n_prosent_sum']
     print('buttons are ready')
-    
+
     while True:
-        if button_left.is_pressed():            
+        if button_left.is_pressed():  
+
             show_on_tower(achived_target)
             show_on_gauge(yoy_growth)
         if button_right.is_pressed():
+
             show_on_tower(0)
             show_on_gauge(0)
-            break
     
-def initialize_spike_module(type, port, attempts=5, delay=5):
+def initialize_sensor(port, attempts=5, delay=5):
     while attempts > 0:
         try:
-            module = type(port)
-            return module
-        except buildhat.exc.BuildHATError:
+            sensor = ForceSensor(port)
+            return sensor
+        except BuildHATError:
             attempts -= 1
             time.sleep(delay)  # Wait for 5 seconds before retrying
     raise  # Re-raise the last exception after all attempts fail
+
+
       
 def get_profundo_data():
     load_dotenv()
--- spike_functions.py
+++ spike_functions.py
@@ -3,7 +3,6 @@ from time import sleep
 
 motor_tower = Motor("A")
 motor_gauge = Motor("B")
-button_reset = ForceSensor('D')
 
 sales_goal = 45000000
 
@@ -17,23 +16,13 @@ def main():
         print(i)
         i += 1
 
-def initialize_spike_module(type, port, attempts=5, delay=5):
-    while attempts > 0:
-        try:
-            module = type(port)
-            return module
-        except buildhat.exc.BuildHATError:
-            attempts -= 1
-            sleep(delay)  # Wait for 5 seconds before retrying
-    raise  # Re-raise the last exception after all attempts fail
-
 def show_on_gauge(degrees):
 
     global first_postion_gauge
     one_degree = 2440 / 90    
     last_position = motor_gauge.get_position()
     target_degrees = first_postion_gauge - last_position - ( degrees * one_degree )
-    motor_gauge.run_for_degrees(target_degrees, 30)
+    motor_gauge.run_for_degrees(target_degrees, 25)
 
 def show_on_tower(per_cent):
 
@@ -43,7 +32,7 @@ def show_on_tower(per_cent):
     one_per_cent = 11700 / 100    
     last_position = motor_tower.get_position()
     target_degrees = first_position_tower - last_position - ( per_cent * one_per_cent )
-    motor_tower.run_for_degrees(target_degrees, 30)
+    motor_tower.run_for_degrees(target_degrees, 35)
 
 if __name__ == '__main__':
     main()
